9ea52c4564f2294d83610c651a31eba827d8f057
crisantogo <christopher.gopeesingh@gmail.com>
2022-07-17 16:54:09 -0400

    created port & tested connection to database

--- server.js
+++ server.js
@@ -19,3 +19,9 @@ MongoClient.connect(dbConnectionString)
         db= client.db(dbName)
         collection= db.collection('comments')
     })
+
+//--CREATE PORT--
+
+app.listen(process.env.PORT||PORT, ()=>{
+    console.log(`Server is running on port = ${process.env.PORT}`)
+})
